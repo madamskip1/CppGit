2d8fbcc418cdd2189feb98c54d2ce3d73b9b889c
Maciej Adamski <madamskip1@gmail.com>
2024-07-18 20:32:24 +0200

    Fix reading from child process

--- src/GitCommandExecutor/GitCommandExecutorUnix.cpp
+++ src/GitCommandExecutor/GitCommandExecutorUnix.cpp
@@ -37,53 +37,47 @@ namespace CppGit
             throw std::runtime_error("Failed to create stderr pipe");
         }
     }
+
     GitCommandOutput GitCommandExecutorUnix::parentProcess()
     {
         close(stdoutPipe[1]);
         close(stderrPipe[1]);
         
-        fd_set readFDs;
-        FD_ZERO(&readFDs);
-        FD_SET(stdoutPipe[0], &readFDs);
-        FD_SET(stderrPipe[0], &readFDs);
-        int maxFD = std::max(stdoutPipe[0], stderrPipe[0]) + 1;
-        if (select(maxFD, &readFDs, nullptr, nullptr, nullptr) == -1)
-        {
-            throw std::runtime_error("Failed to select");
-        }
-
-        char buffer[512];
-
-        std::string stdout;
-        if (FD_ISSET(stdoutPipe[0], &readFDs))
-        {
-            while (read(stdoutPipe[0], buffer, sizeof(buffer)) > 0)
-            {
-                stdout += buffer;
-            }
-        }
-
-        std::string stderr;;
-        if (FD_ISSET(stderrPipe[0], &readFDs))
-        {
-            while (read(stderrPipe[0], buffer, sizeof(buffer)) > 0)
-            {
-                stderr += buffer;
-            }
-        }
-
-        close(stdoutPipe[0]);
-        close(stderrPipe[0]);
-
         int status;
-        if (waitpid(pid, &status, 0) == -1)
+        if (wait(&status) == -1)
         {
-            throw std::runtime_error("Failed to waitpid");
+            throw std::runtime_error("Failed to waitpid.");
         }
 
         int returnCode = WEXITSTATUS(status);
 
-        return GitCommandOutput{returnCode, stdout, stderr};
+        char buffer[256];
+
+        std::string stdoutStr;
+        ssize_t bytesReadStdOut;
+        while ((bytesReadStdOut = read(stdoutPipe[0], buffer, sizeof(buffer) - 1)) > 0)
+        {
+            buffer[bytesReadStdOut] = '\0'; // Null-terminate the buffer correctly
+            stdoutStr += buffer;
+        }
+        if (bytesReadStdOut == -1)
+        {
+            throw std::runtime_error("Failed to read stdout");
+        }
+        
+        std::string stderrStr;
+        ssize_t bytesReadStdErr;
+        while ((bytesReadStdErr = read(stderrPipe[0], buffer, sizeof(buffer) - 1)) > 0)
+        {
+            buffer[bytesReadStdErr] = '\0'; // Null-terminate the buffer correctly
+            stderrStr += buffer;
+        }
+        if (bytesReadStdErr == -1)
+        {
+            throw std::runtime_error("Failed to read stderr");
+        }
+
+        return GitCommandOutput{returnCode, stdoutStr, stderrStr};
     }
 
     void GitCommandExecutorUnix::childProcess(const std::string_view path, const std::string_view command, const std::vector<std::string_view>& args)
