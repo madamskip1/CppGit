02b916ae7e572e232e2787b1cc51e84c036b52f8
Maciej Adamski <madamskip1@gmail.com>
2024-07-18 20:56:11 +0200

    Add method to check if branch exist

--- include/Branches.hpp
+++ include/Branches.hpp
@@ -8,14 +8,22 @@ namespace CppGit
     class Branches
     {
     public:
+        constexpr static const char* const LOCAL_BRANCH_PREFIX = "refs/heads/";
+        constexpr static const char* const REMOTE_BRANCH_PREFIX = "refs/remotes/";
+
         explicit Branches(Repository& repo);
 
         std::vector<Branch> getAllBranches() const;
         std::vector<Branch> getRemoteBranches() const;
         std::vector<Branch> getLocalBranches() const;
 
+        bool branchExists(std::string_view branchName, bool remote = false) const;
+        bool branchExists(const Branch& branch) const;
+
     private:
         std::vector<Branch> getBranchesImpl(bool local, bool remote) const;
+        std::string addPrefixIfNeeded(std::string_view branchName, bool remote) const;
+
         Repository& repo;
     };
 }
--- src/Branches.cpp
+++ src/Branches.cpp
@@ -23,6 +23,19 @@ namespace CppGit
         return getBranchesImpl(true, false);
     }
 
+    bool Branches::branchExists(std::string_view branchName, bool remote) const
+    {
+        auto branchNameWithPrefix = addPrefixIfNeeded(branchName, remote);
+        auto output = repo.executeGitCommand("show-ref", "--verify", "--quiet", branchNameWithPrefix);
+        return output.return_code == 0;
+    }
+
+    bool Branches::branchExists(const Branch &branch) const
+    {
+        // remote = false/true doesn't matter here
+        return branchExists(branch.getRefName(), false);
+    }
+
     std::vector<Branch> Branches::getBranchesImpl(bool local, bool remote) const
     {
         auto argLocal = local ? "refs/heads" : "";
@@ -56,5 +69,24 @@ namespace CppGit
         }
 
         return branches;
+    }
+    std::string Branches::addPrefixIfNeeded(std::string_view branchName, bool remote) const
+    {
+        if (remote)
+        {
+            if (branchName.find("refs/remotes/") == std::string::npos)
+            {
+                return REMOTE_BRANCH_PREFIX + std::string{ branchName };
+            }
+        }
+        else
+        {
+            if (branchName.find("refs/heads/") == std::string::npos)
+            {
+                return LOCAL_BRANCH_PREFIX + std::string{ branchName };
+            }
+        }
+
+        return std::string{ branchName };
     }
 }
