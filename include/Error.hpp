8e31f437f33fbba92a93b6f78c13d6b50337f296
Maciej Adamski <madamskip1@gmail.com>
2024-12-11 19:18:27 +0100

    Change Rebase return/throw to std::expected

--- include/Error.hpp
+++ include/Error.hpp
@@ -10,7 +10,11 @@ enum class Error
     // Cherry-pick
     NO_CHERRY_PICK_IN_PROGRESS,
     CHERRY_PICK_CONFLICT,
-    CHERRY_PICK_EMPTY_COMMIT
+    CHERRY_PICK_EMPTY_COMMIT,
+
+    // Rebase
+    REBASE_CONFLICT,
+    NO_REBASE_IN_PROGRESS,
 };
 
 } // namespace CppGit
