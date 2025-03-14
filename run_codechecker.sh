8aa846ef43783306d161832b2757f481cd61d460
Maciej Adamski <madamskip1@gmail.com>
2025-03-14 17:31:30 +0100

    Add link to repository with own CodeChecker dockerfile

--- run_codechecker.sh
+++ run_codechecker.sh
@@ -2,6 +2,10 @@
 
 REPORTS_DIR=".reports-codechecker"
 
+# This script use custom docker image with codechecker installed
+# You can build it with Dockerfile from this repository
+# https://github.com/madamskip1/CodeChecker-docker
+
 docker run --rm -v $(pwd):$(pwd) -w $(pwd) --user "$(id -u):$(id -g)" --cpus 2 codechecker analyze --config codechecker_config.yml -o $REPORTS_DIR/build/ build/compile_commands.json
 
 docker run --rm -v $(pwd):$(pwd) -w $(pwd) --user "$(id -u):$(id -g)" codechecker parse --export html  -o $REPORTS_DIR/html $REPORTS_DIR/build ; echo "0"
